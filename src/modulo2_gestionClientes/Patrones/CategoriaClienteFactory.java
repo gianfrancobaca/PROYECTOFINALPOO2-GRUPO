dff3f63ec7c37d4d29fd9451ba5ad601665ff0c8
USER5 <enzo_wt@hotmail.com>
2026-06-23 14:47:11 -0500

    diferenciacion de las cateogiras

--- src/modulo2_gestionClientes/Patrones/CategoriaClienteFactory.java
+++ src/modulo2_gestionClientes/Patrones/CategoriaClienteFactory.java
@@ -1,4 +1,19 @@
 package modulo2_gestionClientes.Patrones;
 
+import modulo2_gestionClientes.models.CategoriaCliente;
+
 public class CategoriaClienteFactory {
+
+    public static CategoriaCliente crear(String tipo) {
+        switch (tipo.toUpperCase()) {
+            case "VIP":
+                return new CategoriaCliente(0, "VIP", "Cliente preferencial con maximos beneficios", 20.0);
+            case "REGULAR":
+                return new CategoriaCliente(0, "Regular", "Cliente con compras frecuentes", 10.0);
+            case "NUEVO":
+                return new CategoriaCliente(0, "Nuevo", "Cliente recien registrado", 5.0);
+            default:
+                return new CategoriaCliente(0, "General", "Cliente sin categoria asignada", 0.0);
+        }
+    }
 }
--- src/modulo2_gestionClientes/Patrones/SesionUsuario.java
+++ src/modulo2_gestionClientes/Patrones/SesionUsuario.java
@@ -1,4 +1,35 @@
 package modulo2_gestionClientes.Patrones;
 
+import modulo2_gestionClientes.models.Usuario;
+
 public class SesionUsuario {
+
+    private static SesionUsuario instance;
+    private Usuario usuarioActivo;
+
+    private SesionUsuario() {}
+
+    public static SesionUsuario getInstance() {
+        if (instance == null) {
+            instance = new SesionUsuario();
+        }
+        return instance;
+    }
+
+    public Usuario getUsuarioActivo() {
+        return usuarioActivo;
+    }
+
+    public void setUsuarioActivo(Usuario usuario) {
+        this.usuarioActivo = usuario;
+    }
+
+    public void cerrarSesion() {
+        this.usuarioActivo = null;
+    }
+
+    public boolean haySesionActiva() {
+        return usuarioActivo != null;
+    }
 }
+
