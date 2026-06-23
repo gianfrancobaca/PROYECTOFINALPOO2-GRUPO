10f6a5294c532f30e5052d9a61ff6fa82e2f0797
USER5 <enzo_wt@hotmail.com>
2026-06-23 14:14:34 -0500

    get and set, reclamo

--- src/modulo2_gestionClientes/models/Reclamo.java
+++ src/modulo2_gestionClientes/models/Reclamo.java
@@ -19,3 +19,29 @@ public class Reclamo {
         this.fecha = fecha;
         this.cliente = cliente;
     }
+
+    public int getIdReclamo() { return idReclamo; }
+    public void setIdReclamo(int idReclamo) { this.idReclamo = idReclamo; }
+
+    public String getMotivo() { return motivo; }
+    public void setMotivo(String motivo) { this.motivo = motivo; }
+
+    public String getEstado() { return estado; }
+    public void setEstado(String estado) { this.estado = estado; }
+
+    public Date getFecha() { return fecha; }
+    public void setFecha(Date fecha) { this.fecha = fecha; }
+
+    public Cliente getCliente() { return cliente; }
+    public void setCliente(Cliente cliente) { this.cliente = cliente; }
+
+    @Override
+    public String toString() {
+        return "Reclamo{" +
+                "idReclamo=" + idReclamo +
+                ", motivo='" + motivo + '\'' +
+                ", estado='" + estado + '\'' +
+                ", fecha=" + fecha +
+                '}';
+    }
+}
