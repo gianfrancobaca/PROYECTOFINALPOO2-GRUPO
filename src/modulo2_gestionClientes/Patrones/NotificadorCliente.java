14d01033d0f5a8a66073ab0586c873f3c1dd13ef
USER5 <enzo_wt@hotmail.com>
2026-06-23 14:47:48 -0500

    estados de los pedidos

--- src/modulo2_gestionClientes/Patrones/NotificadorCliente.java
+++ src/modulo2_gestionClientes/Patrones/NotificadorCliente.java
@@ -1,4 +1,23 @@
 package modulo2_gestionClientes.Patrones;
 
-public class NotificadorCliente {
+import modulo2_gestionClientes.models.Cliente;
+import modulo2_gestionClientes.models.Notificacion;
+import java.util.Date;
+
+public class NotificacionFactory {
+
+    public static Notificacion crear(String tipo, Cliente cliente) {
+        switch (tipo.toUpperCase()) {
+            case "BIENVENIDA":
+                return new Notificacion(0, "Bienvenido a nuestro sistema, " + cliente.getNombre(), "BIENVENIDA", new Date(), false, cliente);
+            case "PEDIDO":
+                return new Notificacion(0, "Tu pedido ha sido registrado exitosamente", "PEDIDO", new Date(), false, cliente);
+            case "PAGO":
+                return new Notificacion(0, "Tu pago ha sido procesado correctamente", "PAGO", new Date(), false, cliente);
+            case "RECLAMO":
+                return new Notificacion(0, "Tu reclamo ha sido recibido y sera atendido pronto", "RECLAMO", new Date(), false, cliente);
+            default:
+                return new Notificacion(0, "Tienes una nueva notificacion", "GENERAL", new Date(), false, cliente);
+        }
+    }
 }
