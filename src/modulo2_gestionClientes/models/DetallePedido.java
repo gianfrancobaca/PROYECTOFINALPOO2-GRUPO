6e38b2b9297725fe6304fb27698bea0b2d69c5b1
USER5 <enzo_wt@hotmail.com>
2026-06-23 14:06:07 -0500

    atributos con constructor de detallepedido

--- src/modulo2_gestionClientes/models/DetallePedido.java
+++ src/modulo2_gestionClientes/models/DetallePedido.java
@@ -1,4 +1,18 @@
 package modulo2_gestionClientes.models;
 
 public class DetallePedido {
-}
+    private int idDetalle;
+    private int idPedido;
+    private String producto;
+    private int cantidad;
+    private double precioUnitario;
+
+    public DetallePedido() {}
+
+    public DetallePedido(int idDetalle, int idPedido, String producto, int cantidad, double precioUnitario) {
+        this.idDetalle = idDetalle;
+        this.idPedido = idPedido;
+        this.producto = producto;
+        this.cantidad = cantidad;
+        this.precioUnitario = precioUnitario;
+    }
