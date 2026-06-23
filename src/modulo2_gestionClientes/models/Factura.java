8bd808897b91d7eb4ed9484293fa2c36819f3eb8
USER5 <enzo_wt@hotmail.com>
2026-06-23 14:05:16 -0500

    set and get by: factura class

--- src/modulo2_gestionClientes/models/Factura.java
+++ src/modulo2_gestionClientes/models/Factura.java
@@ -21,3 +21,33 @@ public class Factura {
         this.cliente = cliente;
     }
 
+    public int getIdFactura() { return idFactura; }
+    public void setIdFactura(int idFactura) { this.idFactura = idFactura; }
+
+    public Date getFecha() { return fecha; }
+    public void setFecha(Date fecha) { this.fecha = fecha; }
+
+    public double getTotal() { return total; }
+    public void setTotal(double total) { this.total = total; }
+
+    public String getEstado() { return estado; }
+    public void setEstado(String estado) { this.estado = estado; }
+
+    public Pedido getPedido() { return pedido; }
+    public void setPedido(Pedido pedido) { this.pedido = pedido; }
+
+    public Cliente getCliente() { return cliente; }
+    public void setCliente(Cliente cliente) { this.cliente = cliente; }
+
+    @Override
+    public String toString() {
+        return "Factura{" +
+                "idFactura=" + idFactura +
+                ", fecha=" + fecha +
+                ", total=" + total +
+                ", estado='" + estado + '\'' +
+                '}';
+    }
+}
+
+
