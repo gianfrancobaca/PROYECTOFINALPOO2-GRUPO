dd7ddb704ee2e108d2d2812f620c31645f75f0e7
USER5 <enzo_wt@hotmail.com>
2026-06-23 14:15:44 -0500

    ticketsoporte y programafidelizacion, verificados y corregidos

--- src/modulo2_gestionClientes/models/ProgramaFidelizacion.java
+++ src/modulo2_gestionClientes/models/ProgramaFidelizacion.java
@@ -1,4 +1,44 @@
 package modulo2_gestionClientes.models;
 
 public class ProgramaFidelizacion {
+    private int idPrograma;
+    private String nombre;
+    private int puntosAcumulados;
+    private String nivel;
+    private Cliente cliente;
+
+    public ProgramaFidelizacion() {}
+
+    public ProgramaFidelizacion(int idPrograma, String nombre, int puntosAcumulados, String nivel, Cliente cliente) {
+        this.idPrograma = idPrograma;
+        this.nombre = nombre;
+        this.puntosAcumulados = puntosAcumulados;
+        this.nivel = nivel;
+        this.cliente = cliente;
+    }
+
+    public int getIdPrograma() { return idPrograma; }
+    public void setIdPrograma(int idPrograma) { this.idPrograma = idPrograma; }
+
+    public String getNombre() { return nombre; }
+    public void setNombre(String nombre) { this.nombre = nombre; }
+
+    public int getPuntosAcumulados() { return puntosAcumulados; }
+    public void setPuntosAcumulados(int puntosAcumulados) { this.puntosAcumulados = puntosAcumulados; }
+
+    public String getNivel() { return nivel; }
+    public void setNivel(String nivel) { this.nivel = nivel; }
+
+    public Cliente getCliente() { return cliente; }
+    public void setCliente(Cliente cliente) { this.cliente = cliente; }
+
+    @Override
+    public String toString() {
+        return "ProgramaFidelizacion{" +
+                "idPrograma=" + idPrograma +
+                ", nombre='" + nombre + '\'' +
+                ", puntosAcumulados=" + puntosAcumulados +
+                ", nivel='" + nivel + '\'' +
+                '}';
+    }
 }
--- src/modulo2_gestionClientes/models/TicketSoporte.java
+++ src/modulo2_gestionClientes/models/TicketSoporte.java
@@ -1,4 +1,52 @@
 package modulo2_gestionClientes.models;
 
+import java.util.Date;
+
 public class TicketSoporte {
+    private int idTicket;
+    private String descripcion;
+    private String estado;
+    private String prioridad;
+    private Date fecha;
+    private Reclamo reclamo;
+
+    public TicketSoporte() {}
+
+    public TicketSoporte(int idTicket, String descripcion, String estado, String prioridad, Date fecha, Reclamo reclamo) {
+        this.idTicket = idTicket;
+        this.descripcion = descripcion;
+        this.estado = estado;
+        this.prioridad = prioridad;
+        this.fecha = fecha;
+        this.reclamo = reclamo;
+    }
+
+    public int getIdTicket() { return idTicket; }
+    public void setIdTicket(int idTicket) { this.idTicket = idTicket; }
+
+    public String getDescripcion() { return descripcion; }
+    public void setDescripcion(String descripcion) { this.descripcion = descripcion; }
+
+    public String getEstado() { return estado; }
+    public void setEstado(String estado) { this.estado = estado; }
+
+    public String getPrioridad() { return prioridad; }
+    public void setPrioridad(String prioridad) { this.prioridad = prioridad; }
+
+    public Date getFecha() { return fecha; }
+    public void setFecha(Date fecha) { this.fecha = fecha; }
+
+    public Reclamo getReclamo() { return reclamo; }
+    public void setReclamo(Reclamo reclamo) { this.reclamo = reclamo; }
+
+    @Override
+    public String toString() {
+        return "TicketSoporte{" +
+                "idTicket=" + idTicket +
+                ", descripcion='" + descripcion + '\'' +
+                ", estado='" + estado + '\'' +
+                ", prioridad='" + prioridad + '\'' +
+                ", fecha=" + fecha +
+                '}';
+    }
 }
