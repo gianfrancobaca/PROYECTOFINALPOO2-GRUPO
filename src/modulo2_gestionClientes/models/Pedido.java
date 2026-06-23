211e55e955b26574a0a638480ddfbee4b5fae4a7
USER5 <enzo_wt@hotmail.com>
2026-06-23 13:59:14 -0500

    Code pedido, coloque atributos con su constructor

--- src/modulo2_gestionClientes/models/Pedido.java
+++ src/modulo2_gestionClientes/models/Pedido.java
@@ -1,9 +1,22 @@
 package modulo2_gestionClientes.models;
 
+import java.util.Date;
+import java.util.List;
+
 public class Pedido {
     private int idPedido;
-    private double total;
+    private Date fecha;
+    private String estado;
+    private Cliente cliente;
+    private List<DetallePedido> detalles;
 
-    public Pedido() {
+    public Pedido() {}
+
+    public Pedido(int idPedido, Date fecha, String estado, Cliente cliente, List<DetallePedido> detalles) {
+        this.idPedido = idPedido;
+        this.fecha = fecha;
+        this.estado = estado;
+        this.cliente = cliente;
+        this.detalles = detalles;
     }
-}
+
