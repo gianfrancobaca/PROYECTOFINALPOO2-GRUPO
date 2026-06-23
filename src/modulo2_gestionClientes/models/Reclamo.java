a1f17e877b3619f2a511d19c3714690a530705b7
USER5 <enzo_wt@hotmail.com>
2026-06-23 14:13:21 -0500

    reclamo atributos con construtor

--- src/modulo2_gestionClientes/models/Reclamo.java
+++ src/modulo2_gestionClientes/models/Reclamo.java
@@ -1,10 +1,21 @@
 package modulo2_gestionClientes.models;
 
+import java.util.Date;
+
 public class Reclamo {
     private int idReclamo;
-    private String descripcion;
+    private String motivo;
     private String estado;
+    private Date fecha;
+    private Cliente cliente;
 
     public Reclamo() {
     }
-}
+
+    public Reclamo(int idReclamo, String motivo, String estado, Date fecha, Cliente cliente) {
+        this.idReclamo = idReclamo;
+        this.motivo = motivo;
+        this.estado = estado;
+        this.fecha = fecha;
+        this.cliente = cliente;
+    }
