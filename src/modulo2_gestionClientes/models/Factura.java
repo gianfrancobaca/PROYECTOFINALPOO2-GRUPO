fc213671ecf465b09862af76a5738b4a93f1a065
USER5 <enzo_wt@hotmail.com>
2026-06-23 14:04:00 -0500

    atributos con su contrustor de clase factura

--- src/modulo2_gestionClientes/models/Factura.java
+++ src/modulo2_gestionClientes/models/Factura.java
@@ -1,10 +1,23 @@
 package modulo2_gestionClientes.models;
 
-public class Factura {
-    private int numeroFactura;
-    private String fecha;
-    private double montoTotal;
+import java.util.Date;
 
-    public Factura() {
+public class Factura {
+    private int idFactura;
+    private Date fecha;
+    private double total;
+    private String estado;
+    private Pedido pedido;
+    private Cliente cliente;
+
+    public Factura() {}
+
+    public Factura(int idFactura, Date fecha, double total, String estado, Pedido pedido, Cliente cliente) {
+        this.idFactura = idFactura;
+        this.fecha = fecha;
+        this.total = total;
+        this.estado = estado;
+        this.pedido = pedido;
+        this.cliente = cliente;
     }
-}
+
