ea34d5a4048b2532a4d6c679d3c1d34feb16568b
USER5 <enzo_wt@hotmail.com>
2026-06-23 14:26:32 -0500

    interfaz 1 completa categoriacliente

--- src/modulo2_gestionClientes/Interfaces/ICategoriaClienteRepository.java
+++ src/modulo2_gestionClientes/Interfaces/ICategoriaClienteRepository.java
@@ -1,4 +1,12 @@
-package modulo2_gestionClientes.Interfaces;
+package modulo2_gestionClientes.interfaces;
+
+import modulo2_gestionClientes.models.CategoriaCliente;
+import java.util.List;
 
 public interface ICategoriaClienteRepository {
+    void agregar(CategoriaCliente categoria);
+    void actualizar(CategoriaCliente categoria);
+    void eliminar(int idCategoria);
+    CategoriaCliente buscarPorId(int idCategoria);
+    List<CategoriaCliente> listarTodos();
 }
