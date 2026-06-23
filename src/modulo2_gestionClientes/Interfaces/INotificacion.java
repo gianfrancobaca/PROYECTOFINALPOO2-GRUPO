01327495ab1ce513b3f2ecdb9bfa6cebbedb3dc2
USER5 <enzo_wt@hotmail.com>
2026-06-23 14:28:52 -0500

    notificacion y reporte clinete completos

--- src/modulo2_gestionClientes/Interfaces/INotificacion.java
+++ src/modulo2_gestionClientes/Interfaces/INotificacion.java
@@ -1,4 +1,11 @@
-package modulo2_gestionClientes.Interfaces;
+package modulo2_gestionClientes.interfaces;
+
+import modulo2_gestionClientes.models.Notificacion;
+import java.util.List;
 
 public interface INotificacion {
+    void enviar(Notificacion notificacion);
+    void marcarComoLeida(int idNotificacion);
+    List<Notificacion> listarPorCliente(int idCliente);
+    List<Notificacion> listarNoLeidas(int idCliente);
 }
--- src/modulo2_gestionClientes/Interfaces/IReporteCliente.java
+++ src/modulo2_gestionClientes/Interfaces/IReporteCliente.java
@@ -1,4 +1,10 @@
-package modulo2_gestionClientes.Interfaces;
+package modulo2_gestionClientes.interfaces;
+
+import modulo2_gestionClientes.models.Cliente;
+import java.util.List;
 
 public interface IReporteCliente {
+    List<Cliente> reporteClientesPorCategoria(int idCategoria);
+    List<Cliente> reporteClientesActivos();
+    void generarReportePDF(List<Cliente> clientes);
 }
