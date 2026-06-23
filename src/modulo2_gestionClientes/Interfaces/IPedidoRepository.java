92856ca35c4184777118c1a864e8f5ae5092ed6c
USER5 <enzo_wt@hotmail.com>
2026-06-23 14:27:01 -0500

    pedidorepository

--- src/modulo2_gestionClientes/Interfaces/IPedidoRepository.java
+++ src/modulo2_gestionClientes/Interfaces/IPedidoRepository.java
@@ -1,4 +1,13 @@
-package modulo2_gestionClientes.Interfaces;
+package modulo2_gestionClientes.interfaces;
+
+import modulo2_gestionClientes.models.Pedido;
+import java.util.List;
 
 public interface IPedidoRepository {
+    void agregar(Pedido pedido);
+    void actualizar(Pedido pedido);
+    void eliminar(int idPedido);
+    Pedido buscarPorId(int idPedido);
+    List<Pedido> listarTodos();
+    List<Pedido> listarPorCliente(int idCliente);
 }
