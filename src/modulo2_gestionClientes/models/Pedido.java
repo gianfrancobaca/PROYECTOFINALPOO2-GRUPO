0663b37eca7e5492441fe9dc7dc4b658840aba09
USER5 <enzo_wt@hotmail.com>
2026-06-23 14:00:17 -0500

    pedido, gets y sets

--- src/modulo2_gestionClientes/models/Pedido.java
+++ src/modulo2_gestionClientes/models/Pedido.java
@@ -19,4 +19,30 @@ public class Pedido {
         this.cliente = cliente;
         this.detalles = detalles;
     }
+    public int getIdPedido() { return idPedido; }
+    public void setIdPedido(int idPedido) { this.idPedido = idPedido; }
+
+    public Date getFecha() { return fecha; }
+    public void setFecha(Date fecha) { this.fecha = fecha; }
+
+    public String getEstado() { return estado; }
+    public void setEstado(String estado) { this.estado = estado; }
+
+    public Cliente getCliente() { return cliente; }
+    public void setCliente(Cliente cliente) { this.cliente = cliente; }
+
+    public List<DetallePedido> getDetalles() { return detalles; }
+    public void setDetalles(List<DetallePedido> detalles) { this.detalles = detalles; }
+
+    @Override
+    public String toString() {
+        return "Pedido{" +
+                "idPedido=" + idPedido +
+                ", fecha=" + fecha +
+                ", estado='" + estado + '\'' +
+                ", cliente=" + (cliente != null ? cliente.getNombre() : "Sin cliente") +
+                '}';
+    }
+}
+
 
