e44dc14f6467cdf5e7ee45f6937f7055cbd3c775
USER5 <enzo_wt@hotmail.com>
2026-06-23 15:00:14 -0500

    agrega controlador de categoria cliente

--- src/modulo2_gestionClientes/controllers/CategoriaClienteController.java
+++ src/modulo2_gestionClientes/controllers/CategoriaClienteController.java
@@ -1,4 +1,37 @@
 package modulo2_gestionClientes.controllers;
 
+import modulo2_gestionClientes.models.CategoriaCliente;
+import modulo2_gestionClientes.Patrones.CategoriaClienteFactory;
+import modulo2_gestionClientes.repositories.CategoriaClienteRepository;
+
+import java.util.List;
+
 public class CategoriaClienteController {
+
+    private CategoriaClienteRepository repository;
+
+    public CategoriaClienteController() {
+        this.repository = new CategoriaClienteRepository();
+    }
+
+    public void agregar(String tipo) {
+        CategoriaCliente categoria = CategoriaClienteFactory.crear(tipo);
+        repository.agregar(categoria);
+    }
+
+    public void actualizar(CategoriaCliente categoria) {
+        repository.actualizar(categoria);
+    }
+
+    public void eliminar(int idCategoria) {
+        repository.eliminar(idCategoria);
+    }
+
+    public CategoriaCliente buscarPorId(int idCategoria) {
+        return repository.buscarPorId(idCategoria);
+    }
+
+    public List<CategoriaCliente> listarTodos() {
+        return repository.listarTodos();
+    }
 }
