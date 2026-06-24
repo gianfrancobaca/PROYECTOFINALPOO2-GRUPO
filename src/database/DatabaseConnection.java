4beaf6ec8b6e0f47ea5c2eaf0df7446a8830ba34
Gianfranco Baca <gianfrancobacapol@gmail.com>
2026-06-24 11:26:54 -0500

    Connection

--- src/database/DatabaseConnection.java
+++ src/database/DatabaseConnection.java
@@ -21,7 +21,7 @@ public class DatabaseConnection {
 
     private DatabaseConnection() {
         try {
-            Class.forName("com.mysql.jdbc.Driver");
+            Class.forName("com.mysql.cj.jdbc.Driver");
             this.connection = DriverManager.getConnection(URL, USER, PASSWORD);
             System.out.println("[DB] Conexión establecida con Trapos y Sedas.");
         } catch (ClassNotFoundException | SQLException e) {
