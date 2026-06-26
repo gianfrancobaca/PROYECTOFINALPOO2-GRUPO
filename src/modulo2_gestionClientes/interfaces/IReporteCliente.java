f08d216b919c5b9e0e3e0493ee6fe5be2ad4023a
USER5 <enzo_wt@hotmail.com>
2026-06-26 16:18:19 -0500

    interfaaz reporte cliente corregida

--- src/modulo2_gestionClientes/interfaces/IReporteCliente.java
+++ src/modulo2_gestionClientes/interfaces/IReporteCliente.java
@@ -4,7 +4,10 @@ import modulo2_gestionClientes.models.Cliente;
 import java.util.List;
 
 public interface IReporteCliente {
+
     List<Cliente> reporteClientesPorCategoria(int idCategoria);
+
     List<Cliente> reporteClientesActivos();
-    void generarReportePDF(List<Cliente> clientes);
+
+    String generarReporteTexto(List<Cliente> clientes);
 }
